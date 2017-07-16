d726337dede0e0d88b5ab80fc82c010efc350986
Hsanka6 <haasith2@gmail.com>
2017-07-16 14:53:11 -0700

    Added event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,9 +6,22 @@ import React, {Component} from 'react'
 
 class SearchBar  extends Component
 {
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {term: ''};
+  }
   render ()
   {
-    return <input onChange={(event) => console.log(event.target.value) } />;
+    return(
+      <div>
+      <input onChange={(event) => this.setState({term: event.target.value}) } />
+      </div>
+    );
+    //Value of the input: {this.state.term} // to print out to the screen
+    
+    //return <input onChange={(event) => console.log(event.target.value) } />;
     //return <input onChange={this.onInputChange} />; with function
 
   }
